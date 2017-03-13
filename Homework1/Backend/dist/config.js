ff21759ec9af56bb1c3d5955a0798cbb04b2830c
M Zhou <contact@mzhou.me>
2017-03-12 21:28:23 -0400

    remove keys placeholder

--- Homework1/Backend/dist/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.twitConfig = {
-    consumer_key: '<YOUR CONSUMER KEY>',
-    consumer_secret: '<YOUR CONSUMER SECRET>',
-    access_token: '<YOUR ACCESS TOKEN>',
-    access_token_secret: '<YOUR ACCESS TOKEN SECRET>'
-};
-//# sourceMappingURL=config.js.map
--- Homework1/Backend/src/config.ts
+++
@@ -1,6 +0,0 @@
-export const twitConfig = {
-    consumer_key: '<YOUR CONSUMER KEY>',
-    consumer_secret: '<YOUR CONSUMER SECRET>',
-    access_token: '<YOUR ACCESS TOKEN>',
-    access_token_secret: '<YOUR ACCESS TOKEN SECRET>'
-};
